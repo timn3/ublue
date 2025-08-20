5b1859316f2d1deda3d46ee14b90196a66cb633e
Tim <niertim308@gmail.com>
2025-08-21 00:01:10 +0200

    another attempt

--- build_files/build.sh
+++ build_files/build.sh
@@ -46,8 +46,8 @@ rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
 
 dnf check-update -y
-dnf install -y \
-    code
+# dnf install -y \
+#     code
 
     # btop \
 
