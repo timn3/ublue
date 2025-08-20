34f3529763aa8b73c392665cb26ee9a30cd1cc26
Tim <niertim308@gmail.com>
2025-08-20 23:51:57 +0200

    attempt on fixing pipeline

--- build_files/build.sh
+++ build_files/build.sh
@@ -52,83 +52,83 @@ dnf install -y \
     # btop \
 
 # desktop plumbing/apps
-dnf install -y \
-    dejavu-sans-fonts \
-    dejavu-serif-fonts \
-    dejavu-sans-mono-fonts \
-    flatpak \
-    fontawesome-fonts \
-    google-noto-emoji-color-fonts \
-    gvfs-mtp \
-    pulseaudio-utils \
-    alsa-plugins-pulseaudio \
-    gstreamer1-plugins-good \
-    gstreamer1-plugins-bad-free \
-    gstreamer1-plugin-openh264 \
-    gstreamer1-libav \
-    libavdevice \
-    xdg-desktop-portal-gtk \
-    pavucontrol \
-    pcmanfm \
-    nm-connection-editor \
-    eog \
-    evince \
-    rhythmbox \
-    gnome-keyring \
-    pinentry-gnome3 \
-    mate-polkit \
-    lxterminal \
-    gnome-disk-utility \
-    rofimoji
+# dnf install -y \
+#     dejavu-sans-fonts \
+#     dejavu-serif-fonts \
+#     dejavu-sans-mono-fonts \
+#     flatpak \
+#     fontawesome-fonts \
+#     google-noto-emoji-color-fonts \
+#     gvfs-mtp \
+#     pulseaudio-utils \
+#     alsa-plugins-pulseaudio \
+#     gstreamer1-plugins-good \
+#     gstreamer1-plugins-bad-free \
+#     gstreamer1-plugin-openh264 \
+#     gstreamer1-libav \
+#     libavdevice \
+#     xdg-desktop-portal-gtk \
+#     pavucontrol \
+#     pcmanfm \
+#     nm-connection-editor \
+#     eog \
+#     evince \
+#     rhythmbox \
+#     gnome-keyring \
+#     pinentry-gnome3 \
+#     mate-polkit \
+#     lxterminal \
+#     gnome-disk-utility \
+#     rofimoji
 
-# sway/wayland desktop
-dnf install -y \
-    sway \
-    swayidle \
-    swaylock \
-    kanshi \
-    mako \
-    waybar \
-    slurp \
-    grim \
-    xorg-x11-server-Xwayland \
-    firefox \
-    wofi \
-    brightnessctl \
-    wl-clipboard
+# # sway/wayland desktop
+# dnf install -y \
+#     sway \
+#     swayidle \
+#     swaylock \
+#     kanshi \
+#     mako \
+#     waybar \
+#     slurp \
+#     grim \
+#     xorg-x11-server-Xwayland \
+#     firefox \
+#     wofi \
+#     brightnessctl \
+#     wl-clipboard
 
-dnf remove -y \
-    NetworkManager-cloud-setup \
-    adcli \
-    avahi \
-    btrfs-progs \
-    chrony \
-    cloud-utils-growpart \
-    fwupd \
-    libosinfo \
-    lvm2 \
-    lvm2-libs \
-    nfs-utils \
-    nilfs-utils \
-    passim-libs \
-    quota-nls \
-    rpcbind \
-    sssd-common \
-    sssd-nfs-idmap \
-    sos \
-    tracker \
-    tracker-miners \
-    udftools \
-    vim-data \
-    vim-minimal \
-    xdg-desktop-portal-wlr \
-    zram-generator
+# dnf remove -y \
+#     NetworkManager-cloud-setup \
+#     adcli \
+#     avahi \
+#     btrfs-progs \
+#     chrony \
+#     cloud-utils-growpart \
+#     fwupd \
+#     libosinfo \
+#     lvm2 \
+#     lvm2-libs \
+#     nfs-utils \
+#     nilfs-utils \
+#     passim-libs \
+#     quota-nls \
+#     rpcbind \
+#     sssd-common \
+#     sssd-nfs-idmap \
+#     sos \
+#     tracker \
+#     tracker-miners \
+#     udftools \
+#     vim-data \
+#     vim-minimal \
+#     xdg-desktop-portal-wlr \
+#     zram-generator
 
-rpm -qa 'qemu-user-static*' | xargs dnf remove -y
+# rpm -qa 'qemu-user-static*' | xargs dnf remove -y
 
-dnf clean all
+# dnf clean all
 
-rpm -e --verbose dnf dnf-data python3-dnf
+# rpm -e --verbose dnf dnf-data python3-dnf
 
 # Default terminal tools
 # dnf5 install -y zsh ptyxis
@@ -143,4 +143,4 @@ rpm -e --verbose dnf dnf-data python3-dnf
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
