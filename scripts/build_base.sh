4f9dc48dc3596f8849c3d0d37b16ad749d206f10
timn3 <niertim308@gmail.com>
2025-12-06 18:52:52 +0100

    Added test flatpaks

--- scripts/build_base.sh
+++ scripts/build_base.sh
@@ -12,6 +12,8 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y tmux 
 
+flatpak install org.signal.Signal com.spotify.Client
+
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
