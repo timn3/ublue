22fe403b263997bc31759149deb90d07883d038b
timn3 <niertim308@gmail.com>
2025-12-10 22:15:27 +0100

    Reinstall flatpaks only if list changed

--- system_files/usr/local/bin/flatpak-user-install.sh
+++ system_files/usr/local/bin/flatpak-user-install.sh
@@ -2,22 +2,26 @@
 set -euo pipefail
 
 LIST_FILE="/usr/share/flatpak/flatpaks.txt"
-# RUN_MARKER="$HOME/.local/share/flatpak/.bootc-flatpak-installed"
+HASH_FILE="$HOME/.local/share/flatpak/.bootc-flatpak-installed"
+OLD_HASH=$(cat "$HASH_FILE")
+NEW_HASH=$(sha256sum "$LIST_FILE" | awk '{print $1}')
 
-# If we've already run once, exit
-# if [[ -f "$RUN_MARKER" ]]; then
-#     exit 0
-# fi
 
+if [[ "$OLD_HASH" == "$NEW_HASH" ]]; then
+    echo "Flatpaks installed, nothing to do."
+    exit 0
+fi
+
+echo "Flatpak list changed, re-installing flatpaks."
 # Ensure Flathub exists
 flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak uninstall -y --all
 # Install each Flatpak
 while IFS= read -r app || [[ -n "$app" ]]; do
     [[ -z "$app" ]] && continue
-    flatpak install --or-update -y --user flathub "$app"
+    flatpak install -y --user flathub "$app"
 done < "$LIST_FILE"
 
-
-# mkdir -p "$(dirname "$RUN_MARKER")"
-# touch "$RUN_MARKER"
+mkdir -p "$(dirname "$HASH_FILE")"
+touch "$HASH_FILE"
+echo "$NEW_HASH" > "$HASH_FILE"
