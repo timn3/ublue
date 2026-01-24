e1d822ccf76a62277c10dd12aaec2f18c8bcf89e
Tim <niertim308@gmail.com>
2026-01-24 10:33:02 +0100

    add proper condition

--- system_files/usr/local/bin/bootc-check-update.sh
+++ system_files/usr/local/bin/bootc-check-update.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+
+if sudo bootc upgrade --check | awk '{ $1$2 == "Nochanges"}'; then
+    echo "Exit"
+    exit 0
+fi
+
 notify-send \
  --urgency=normal \
  --icon=system-software-update \
  "System Update Ready" \
  "A system update has been is available. Run 'sudo bootc upgrade' to apply it."
-
-sudo bootc upgrade --check
