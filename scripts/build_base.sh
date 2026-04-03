28dacd9e52cf445e4a6eab113eda62f999fe7b34
Tim <niertim308@gmail.com>
2026-04-04 00:34:57 +0200

    switch miracle for sway

--- scripts/build_base.sh
+++ scripts/build_base.sh
@@ -65,9 +65,10 @@ sh /ctx/scripts/install_scripts/install-eza.sh
 # Install cosign
 sh /ctx/scripts/install_scripts/install-cosign.sh
 
-### Install miracle WM
-dnf5 -y install @miraclewm-desktop-environment
-
+### Install sway
+dnf5 -y install sway swaybg swayidle swaylock eww wofi grim slurp mako blueman nm-applet tuned-switcher wlroots
+dnf5 -y install sway-config-upstream
+dnf5 -y install powertop
 
 # Install starship
 dnf5 -y copr enable atim/starship
