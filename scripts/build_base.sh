3e84e01b065ceaf53dcb686e3be107fa0eb7d4ca
Tim <niertim308@gmail.com>
2026-04-07 20:09:38 +0200

    added more sway deps

--- scripts/build_base.sh
+++ scripts/build_base.sh
@@ -66,7 +66,7 @@ sh /ctx/scripts/install_scripts/install-eza.sh
 sh /ctx/scripts/install_scripts/install-cosign.sh
 
 ### Install sway
-dnf5 -y install sway swaybg swayidle swaylock eww wofi grim slurp mako blueman nm-applet tuned-switcher wlroots
+dnf5 -y install sway swaybg swayidle swaylock eww wofi grim slurp mako blueman nm-applet tuned-switcher wlroots nmtui brightnessctl wayfire
 dnf5 -y install sway-config-upstream
 dnf5 -y install powertop
 
