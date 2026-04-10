55f29ba0b722dcc95aa5c726dd859675b41d2f2d
Tim <niertim308@gmail.com>
2026-04-10 13:37:49 +0200

    Fix again

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,7 +2,7 @@
 set -ouex pipefail
 
 # got to /etc/yum.repos.d and remove the copr repos
-rm /etc/yum.repos.d/copr:*
+# rm /etc/yum.repos.d/copr:*
 rm /etc/yum.repos.d/_copr:*
 rm /etc/yum.repos.d/google-chrome.repo
 rm /etc/yum.repos.d/terra.repo
