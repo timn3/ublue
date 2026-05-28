9be208b8487cec50a83d73976845b7a282c817a4
Tim <niertim308@gmail.com>
2026-05-28 19:36:07 +0200

    test to fix this

--- scripts/install_scripts/install-vpn.sh
+++ scripts/install_scripts/install-vpn.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+rm /opt
+mkdir /opt
+
 # Make opt persistent across reboots
 systemctl enable ostree-state-overlay@opt.service --now
 
 # Some ostree/bootc images have /opt -> /var/opt.
-if [[ -L /opt ]]; then
-    mkdir -p "$(readlink -m /opt)"
-else
-    mkdir -p /opt
-fi
+# if [[ -L /opt ]]; then
+#     mkdir -p "$(readlink -m /opt)"
+# else
+#     mkdir -p /opt
+# fi
 
 # Install Cisco Secure Client VPN
 dnf5 -y install /ctx/binaries/cisco-secure-client-vpn-5.1.17.3382-1.x86_64.rpm
